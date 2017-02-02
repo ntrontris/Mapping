a307c395cd6ff1493d475d3266d69561254dd333
haduken <ntrontris1@gmail.com>
2017-02-02 13:00:44 +0000

    Latitude: 40.1 and longitude 22.5 (This is Litochoro, Greece - near Mount Olympus)

--- app/src/main/java/com/example/a2mitsf51/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2mitsf51/mapping/HelloMap.java
@@ -6,8 +6,14 @@ import android.preference.PreferenceManager;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
-public class HelloMap extends Activity
+
+public class HelloMap extends Activity implements View.OnClickListener
 {
 
     MapView mv;
@@ -18,6 +24,9 @@ public class HelloMap extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
+
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
         Configuration.getInstance().load
@@ -27,6 +36,17 @@ public class HelloMap extends Activity
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14); //street view
-        mv.getController().setCenter(new GeoPoint(40.1,22.5)); 
+        mv.getController().setCenter(new GeoPoint(40.1,22.5));
+    }
+
+    @Override
+    public void onClick(View view) {
+        double latitude = 50.89;
+        double longitude = 1.39;
+
+        EditText latitudeEditBox = (TextView) findViewById(latitude);
+        EditText longitude
+
+        mv.getController().setCenter(new GeoPoint(latitude, longitude));
     }
 }
